03e090e3d722d1ccf16f31083c6164316d6c870e
Erick Kurniawan <erick.kurniawan@gmail.com>
2019-10-24 11:29:44 +0700

    menambahkan gapi

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,14 @@ import StreamDelete from './StreamDelete';
 import StreamEdit from './StreamEdit';
 import StreamList from './StreamList';
 import StreamShow from './StreamShow';
+import Header from './Header';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
+            <Header />
             <Route path="/" exact component={StreamList} />
             <Route path="/streams/new" component={StreamCreate} />
             <Route path="/streams/edit" component={StreamEdit} />
